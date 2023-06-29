1c142e0cd363ecaa8d98f17e95768c6bbf5b57ca
Jakub Kosakowski <jkosakowski602@gmail.com>
2023-06-29 14:36:03 +0200

    Create edit GET method

--- app.py
+++ app.py
@@ -29,5 +29,22 @@ def add():
     if request.method == 'GET':
         return render_template('add.html')
 
+
+@app.route('/edit/<int:id>', methods=['GET', 'POST'])
+def edit(id):
+    data = exec_sql("""
+        SELECT * FROM People
+        WHERE id = ?;
+    """, (id,))
+    person = {
+        'id': data[0][0],
+        'age': data[0][1],
+        'height': data[0][2],
+        'gender': data[0][3],
+        'favorite_color': data[0][4]
+    }
+    if request.method == 'GET':
+        return render_template('edit.html', person=person)
+
 if __name__ == "__main__":
     app.run(debug=True)
