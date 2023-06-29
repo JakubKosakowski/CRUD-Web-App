0bab877120d28abf3dd53d08d6a6f5ecb067fa3c
Jakub Kosakowski <jkosakowski602@gmail.com>
2023-06-29 12:20:56 +0200

    Create exec_sql function and show test data on page

--- app.py
+++ app.py
@@ -1,11 +1,28 @@
 from flask import Flask, render_template
+from database_execute import exec_sql
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def home():
-    return render_template('home.html')
+    datas = exec_sql(
+        """
+        SELECT * FROM People;
+        """
+    )
+    people = list()
+    for data in datas:
+        temp = {
+            'id': data[0],
+            'age': data[1],
+            'height': data[2],
+            'gender': data[3],
+            'favorite_color': data[4]
+        }
+        people.append(temp)
+
+    return render_template('home.html', people=people)
 
 
 if __name__ == "__main__":
