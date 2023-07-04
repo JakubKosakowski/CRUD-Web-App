eae91b514ef68f1e567666b8e96ac25596c71059
Jakub Kosakowski <jkosakowski602@gmail.com>
2023-07-04 13:26:54 +0200

    Refactor entire app

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template, request, url_for, redirect
+from flask import Flask, render_template, request, url_for, redirect, flash, get_flashed_messages
 from models import db, SurveyModel
 
 app = Flask(__name__)
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
@@ -28,13 +29,22 @@ def add():
 
         """
     if request.method == 'GET':
-        return render_template('add.html')
+        return render_template('form.html', form_type="add", title="Add new survey")
     else:
         name = request.form.get('name')
         age = request.form.get('age')
         height = request.form.get('height')
         gender = request.form.get('gender')
         fav_color = request.form.get('favorite_color')
+        if int(age) < 0:
+            flash("Age cannot be lower then 0!")
+        if int(height) < 0:
+            flash("Height cannot be lower then 0!")
+        if any(char.isdigit() for char in name):
+            flash("Name cannot contain any numbers!")
+        messages = get_flashed_messages()
+        if messages:
+            return render_template('add.html', form_type="add", title="Add new survey")
         survey = SurveyModel(name=name, age=age, height=height, gender=gender, favorite_color=fav_color)
         db.session.add(survey)
         db.session.commit()
@@ -69,21 +79,24 @@ def edit(id):
     survey = SurveyModel.query.filter_by(id=id).first()
     if request.method == 'POST':
         if survey:
-            db.session.delete(survey)
-            db.session.commit()
-
-            name = request.form.get('name')
-            age = request.form.get('age')
-            height = request.form.get('height')
-            gender = request.form.get('gender')
-            fav_color = request.form.get('favorite_color')
-            survey = SurveyModel(name=name, age=age, height=height, gender=gender, favorite_color=fav_color)
-
-            db.session.add(survey)
+            if int(request.form.get('age')) < 0:
+                flash("Age cannot be lower then 0!")
+            if int(request.form.get('height')) < 0:
+                flash("Height cannot be lower then 0!")
+            if any(char.isdigit() for char in request.form.get('name')):
+                flash("Name cannot contain any numbers!")
+            messages = get_flashed_messages()
+            if messages:
+                return render_template('form.html', form_type="edit", title="Edit existed survey", survey=survey)
+            survey.name = request.form.get('name')
+            survey.age = request.form.get('age')
+            survey.height = request.form.get('height')
+            survey.gender = request.form.get('gender')
+            survey.fav_color = request.form.get('favorite_color')
             db.session.commit()
             return redirect(url_for('details', id=id))
         return f'Survey with id = {id} does not exist'
-    return render_template('edit.html', survey=survey)
+    return render_template('form.html', form_type="edit", title="Edit existed survey", survey=survey)
 
 
 @app.route('/delete/<int:id>', methods=['GET', 'POST'])
